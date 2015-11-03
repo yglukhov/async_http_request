e7d36f1e5e87ada14f9b70ab3f1fe1e461a5daa4
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2015-11-03 17:24:55 +0200

    Use nimcall handler for threaded request.

--- async_http_request.nim
+++ async_http_request.nim
@@ -1,5 +1,9 @@
 type Response* = tuple[status: string, body: string]
-type Handler* = proc (data: Response)
+
+when defined(js):
+    type Handler* = proc (data: Response)
+else:
+    type Handler* = proc (data: Response, ctx: pointer) {.nimcall.}
 
 when not defined(js):
     import asyncdispatch, httpclient
@@ -15,18 +19,26 @@ when not defined(js):
         extraHeaders: string
         body: string
         handler: Handler
+        ctx: pointer
 
     proc ayncHTTPRequest(a: ThreadArg) {.thread.} =
         try:
             let resp = request(a.url, "http" & a.httpMethod, a.extraHeaders, a.body, sslContext = nil)
-            a.handler((resp.status, resp.body))
+            a.handler((resp.status, resp.body), a.ctx)
         except:
             echo "Exception caught: ", getCurrentExceptionMsg()
             echo getCurrentException().getStackTrace()
 
-proc sendRequestThreaded*(meth, url, body: string, headers: openarray[(string, string)], handler: Handler) =
-    ## handler might not be called on the invoking thread
-    when defined(js):
+    proc sendRequestThreaded*(meth, url, body: string, headers: openarray[(string, string)], handler: Handler, ctx: pointer = nil) =
+        ## handler might not be called on the invoking thread
+        var t = Thread[ThreadArg].new()
+        var extraHeaders = ""
+        for h in headers:
+            extraHeaders &= h[0] & ": " & h[1] & "\r\n"
+        createThread(t[], ayncHTTPRequest, ThreadArg(url: url, httpMethod: meth, extraHeaders: extraHeaders, body: body, handler: handler, ctx: ctx))
+
+when defined(js):
+    proc sendRequestThreaded*(meth, url, body: string, headers: openarray[(string, string)], handler: Handler) =
         let cmeth : cstring = meth
         let curl : cstring = url
         var cbody : cstring
@@ -52,18 +64,6 @@ proc sendRequestThreaded*(meth, url, body: string, headers: openarray[(string, s
             oReq.send(`cbody`);
         }
         """.}
-    else:
-        var t : ref Thread[ThreadArg]
-        t.new()
-
-        var extraHeaders = ""
-        for h in headers:
-            extraHeaders &= h[0] & ": " & h[1] & "\r\n"
-        createThread(t[], ayncHTTPRequest, ThreadArg(url: url, httpMethod: meth, extraHeaders: extraHeaders, body: body, handler: handler))
-
-when defined(js):
-    template sendRequest*(meth, url, body: string, headers: openarray[(string, string)], handler: Handler) =
-        sendRequestThreaded(meth, url, body, headers, handler)
 
     template sendRequest*(meth, url, body: string, headers: openarray[(string, string)], handler: proc(body: string)) =
         sendRequestThreaded(meth, url, body, headers, proc(r: Response) = handler(r.body))
