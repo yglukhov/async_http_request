1bc1cbb1bf110f3abb43ebe6130c7309a20f2f42
Yuriy Glukhov <yutiy.glukhov@gmail.com>
2016-06-23 17:55:33 +0300

    Rewrote XMLHTTPRequest via jsbind

--- async_http_request.nim
+++ async_http_request.nim
@@ -6,73 +6,45 @@ type Response* = tuple[status: string, body: string]
 
 type Handler* = proc (data: Response)
 
-when defined(emscripten):
-    import emscripten
+when defined(emscripten) or defined(js):
+    import jsbind
+    type
+        XMLHTTPRequest* = ref object of JSObj
 
-    type Context = ref object
-        handler: Handler
-        resp: Response
+    proc newXMLHTTPRequest*(): XMLHTTPRequest {.jsimportgWithName: "function(){return (window.XMLHttpRequest)?new XMLHttpRequest():new ActiveXObject('Microsoft.XMLHTTP')}".}
 
-    proc allocHttpResponse(hw: pointer, statusSize, bodySize: cint) {.EMSCRIPTEN_KEEPALIVE.} =
-        let ctx = cast[Context](hw)
-        ctx.resp.status = newString(statusSize)
-        ctx.resp.body = newString(bodySize)
-    proc getHttpResponseStatusPtr(hw: pointer): pointer {.EMSCRIPTEN_KEEPALIVE.} =
-        addr(cast[Context](hw).resp.status[0])
-    proc getHttpResponseBodyPtr(hw: pointer): pointer {.EMSCRIPTEN_KEEPALIVE.} =
-        addr(cast[Context](hw).resp.body[0])
-    proc onAsyncHttpRequestLoad(hw: pointer) {.EMSCRIPTEN_KEEPALIVE.} =
-        let ctx = cast[Context](hw)
-        ctx.handler(ctx.resp)
-        GC_unref(ctx)
+    proc open*(r: XMLHTTPRequest, httpMethod, url: cstring) {.jsimport.}
+    proc send*(r: XMLHTTPRequest) {.jsimport.}
+    proc send*(r: XMLHTTPRequest, body: cstring) {.jsimport.}
+
+    proc addEventListener*(r: XMLHTTPRequest, event: cstring, listener: proc()) {.jsimport.}
+    proc setRequestHeader*(r: XMLHTTPRequest, header, value: cstring) {.jsimport.}
+
+    proc responseText*(r: XMLHTTPRequest): jsstring {.jsimportProp.}
+    proc statusText*(r: XMLHTTPRequest): jsstring {.jsimportProp.}
+
+    proc `responseType=`*(r: XMLHTTPRequest, t: cstring) {.jsimportProp.}
+    proc response*(r: XMLHTTPRequest): JSObj {.jsimportProp.}
 
     proc sendRequest*(meth, url, body: string, headers: openarray[(string, string)], handler: Handler) =
-        var h = ""
-        for kv in headers:
-            h &= kv[0]
-            h &= ":"
-            h &= kv[1]
-            h &= "\l"
-        var hw = Context.new()
-        hw.handler = handler
-        GC_ref(hw)
+        let oReq = newXMLHTTPRequest()
+        var reqListener: proc()
+        reqListener = proc () =
+            handler(($oReq.statusText,  $oReq.responseText))
+            jsUnref(reqListener)
+        jsRef(reqListener)
+        oReq.responseType = "text"
+        oReq.addEventListener("load", reqListener)
+        oReq.open(meth, url)
+        for h in headers:
+            oReq.setRequestHeader(h[0], h[1])
+        if body.isNil:
+            oReq.send()
+        else:
+            oReq.send(body)
 
-        discard EM_ASM_INT("""
-        var meth = Pointer_stringify($0);
-        var url = Pointer_stringify($1);
-        var body = ($2 === 0) ? null : Pointer_stringify($2);
-        var h = Pointer_stringify($3);
-        var hw = $4;
-
-        var oReq;
-        if (window.XMLHttpRequest)
-            oReq = new XMLHttpRequest();
-        else
-            oReq = new ActiveXObject("Microsoft.XMLHTTP");
-        oReq.responseType = "arraybuffer";
-
-        oReq.open(meth, url);
-        var headers = h.split("\n");
-        for(var i = 0; i < headers.length; i++) {
-            if (headers[i].length != 0) {
-                var kv = headers[i].split(":");
-                oReq.setRequestHeader(kv[0], kv[1]);
-            }
-        }
-
-        oReq.addEventListener("load", function(e) {
-            var byteArray = new Uint8Array(oReq.response);
-            _allocHttpResponse(hw, 5, byteArray.length); // TODO: Complete Status!
-            var dataPtr = _getHttpResponseBodyPtr(hw);
-            HEAPU8.set(byteArray, dataPtr);
-            _onAsyncHttpRequestLoad(hw);
-        });
-        if (body === null)
-            oReq.send();
-        else
-            oReq.send(body);
-
-        """, meth.cstring, url.cstring, body.cstring, h.cstring, cast[pointer](hw))
+    template sendRequest*(meth, url, body: string, headers: openarray[(string, string)], handler: proc(body: string)) =
+        sendRequest(meth, url, body, headers, proc(r: Response) = handler(r.body))
 
 elif not defined(js):
     import asyncdispatch, httpclient
@@ -119,48 +91,3 @@ elif not defined(js):
         proc sendRequestThreaded*(meth, url, body: string, headers: openarray[(string, string)], handler: ThreadedHandler, ctx: pointer = nil) =
             ## handler might not be called on the invoking thread
             spawn asyncHTTPRequest(url, meth, genHeaders(body, headers), body, handler, ctx)
-
-when defined(js):
-    type
-        XMLHTTPRequest* = ref XMLHTTPRequestObj
-        XMLHTTPRequestObj {.importc.} = object
-            responseType*: cstring
-
-    proc open*(r: XMLHTTPRequest, httpMethod, url: cstring) {.importcpp.}
-    proc send*(r: XMLHTTPRequest) {.importcpp.}
-    proc send*(r: XMLHTTPRequest, body: cstring) {.importcpp.}
-    proc addEventListener*(r: XMLHTTPRequest, event: cstring, listener: proc(e: ref RootObj)) {.importcpp.}
-    proc addEventListener*(r: XMLHTTPRequest, event: cstring, listener: proc()) {.importcpp.}
-    proc setRequestHeader*(r: XMLHTTPRequest, header, value: cstring) {.importcpp.}
-
-    proc newXMLHTTPRequest*(): XMLHTTPRequest =
-        {.emit: """
-        if (window.XMLHttpRequest)
-            `result` = new XMLHttpRequest();
-        else
-            `result` = new ActiveXObject("Microsoft.XMLHTTP");
-        """.}
-
-    proc sendRequest*(meth, url, body: string, headers: openarray[(string, string)], handler: Handler) =
-        let reqListener = proc (r: ref RootObj) =
-            var cbody: cstring
-            var cstatus: cstring
-            {.emit: """
-            `cbody` = `r`.target.responseText;
-            `cstatus` = `r`.target.statusText;
-            """.}
-            handler(($cstatus,  $cbody))
-
-        let oReq = newXMLHTTPRequest()
-        oReq.responseType = "text"
-        oReq.addEventListener("load", reqListener)
-        oReq.open(meth, url)
-        for h in headers:
-            oReq.setRequestHeader(h[0], h[1])
-        if body.isNil:
-            oReq.send()
-        else:
-            oReq.send(body)
-
-    template sendRequest*(meth, url, body: string, headers: openarray[(string, string)], handler: proc(body: string)) =
-        sendRequest(meth, url, body, headers, proc(r: Response) = handler(r.body))
